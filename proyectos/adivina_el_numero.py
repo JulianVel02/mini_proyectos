afb3a4bc72ed40bd9ffed7732f1f744f5e427f12
JulianVel02 <julianvelazquezas@gmail.com>
2024-05-22 02:08:07 -0300

    Update adivina_el_numero.py

---
+++ proyectos/adivina_el_numero.py
@@ -0,0 +1,8 @@
+from modulos.funciones_adivina_numero import *
+X = 20
+
+bienvenida_al_juego()
+adivina_el_numero(X)
+turno_computadora()
+bienvenida_al_juego_computadora(X)
+adivina_numero_computadora(X)
